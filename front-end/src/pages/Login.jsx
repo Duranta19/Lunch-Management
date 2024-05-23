53a6b392c3a4c308373499a958be9ec15dac10ee
Sayem Siam <sayemsiam198@gmail.com>
2024-05-23 13:08:12 +0600

    Add login fr-ed

--- front-end/src/pages/Login.jsx
+++ front-end/src/pages/Login.jsx
@@ -6,10 +6,9 @@ function App() {
   return (
     <>
       <Nav 
-        li= {["Add Menu", "View Employee Choice", "Logout"]}
+        li= {["home", "Logout"]}
       />
-      
-     
+      <LoginForm />
     </>
   );
 }
