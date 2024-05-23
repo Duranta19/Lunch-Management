290f1acb8b80538807141f5dbd73a8077d100e37
Sayem Siam <sayemsiam198@gmail.com>
2024-05-23 14:56:16 +0600

    view menu for employee

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,14 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
-import Nav from './components/Nav';
-import EmployeeChoiceList from './components/EmployeeChoiceList';
+import logo from "./logo.svg";
+import "./App.css";
+import Nav from "./components/Nav";
+import ItemCard from "./components/ItemCard";
 function App() {
   return (
     <>
-      <Nav 
-        li= {["Add Menu", "View Employee Choice", "Logout"]}
-      />
-      <EmployeeChoiceList />
+      <Nav li={["View Menu", "My Choice", "Logout"]} />
+
+      <div class="p-5 grid grid-cols-1 mx-auto md:grid-cols-3 gap-3 lg:grid-cols-4">
+      <ItemCard />
+      <ItemCard />
+      <ItemCard />
+      <ItemCard />
+      <ItemCard />
+      <ItemCard />
+      </div>
 
     </>
   );
