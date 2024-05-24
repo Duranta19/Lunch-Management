5a885f8edb23cef897144b262dfe490efd4b641d
Sayem Siam <sayemsiam198@gmail.com>
2024-05-25 03:14:48 +0600

    auth-user fix signup form

--- front-end/src/components/SigninForm.jsx
+++ front-end/src/components/SigninForm.jsx
@@ -47,21 +47,7 @@ const SigninForm = () => {
             type="password"
             id="repeat-password"
             name="repeat-password"
-            className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
-            required
-          />
-        </div>
-        <div class="mb-5">
-          <label
-            for="repeat-password"
-            class="block mb-2 text-sm font-medium text-gray-900 "
-          >
-            Repeat password
-          </label>
-          <input
-            type="password"
-            id="repeat-password"
-            name="repeat-password"
+            placeholder="Repeat password"
             className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
             required
           />
