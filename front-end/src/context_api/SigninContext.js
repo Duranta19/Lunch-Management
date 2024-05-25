c9a549ecc62b63250d61bfc61822c094d792487c
Sayem Siam <sayemsiam198@gmail.com>
2024-05-25 23:21:41 +0600

    signin connect with client

---
+++ front-end/src/context_api/SigninContext.js
@@ -0,0 +1,46 @@
+import axios from "axios";
+import React, { createContext, useContext, useState } from "react";
+import { useNavigate } from 'react-router-dom';
+export const SigninContext = createContext("");
+const SigninProvider = ({ children }) => {
+    const [formData, setFormData] = useState({
+        user_name: "",
+        password: "",
+    });
+    const navigate = useNavigate()
+    const handelInput = (e) =>{
+        setFormData({
+            ...formData,
+            [e.target.name]: e.target.value,
+        })
+    };
+    const submitHandelar = async (e) => {
+        e.preventDefault();
+        try {
+            const signinResposnse = await axios.post('http://localhost:3001/signin/', formData);
+            console.log(signinResposnse.data.msg);
+            if (signinResposnse.data.msg == 'admin') {
+                navigate('/admin');
+            } else {
+                navigate('/employee')
+            }
+
+        } catch (error) {
+            console.error(error);
+        }
+    }
+  const SigninContextValue = {
+    formData,
+    submitHandelar,
+    handelInput,
+  };
+  return (
+    <div>
+      <SigninContext.Provider value={SigninContextValue}>
+        {children}
+      </SigninContext.Provider>
+    </div>
+  );
+};
+
+export default SigninProvider;
--- front-end/src/pages/Login.jsx
+++ front-end/src/pages/Login.jsx
@@ -2,6 +2,7 @@ import logo from '../logo.svg';
 import '../App.css';
 import Nav from '../components/Nav';
 import LoginForm from '../components/LoginForm';
+import SigninProvider from '../context_api/SigninContext';
 function Login() {
   return (
     <>
@@ -9,7 +10,10 @@ function Login() {
         li= {[{label:"Signup", href: "/signup"}]}
       />
       <span className='flex underline text-4xl mx-auto items-center justify-center mt-5'>Login</span>
-      <LoginForm />
+      <SigninProvider>
+        <LoginForm />
+      </SigninProvider>
+      
     </>
   );
 }
