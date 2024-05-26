56e81733bb6d66030873ef9b2a18673e2f841c57
Sayem Siam <sayemsiam198@gmail.com>
2024-05-26 11:35:16 +0600

    merge with admin-connect-client

---
+++ front-end/src/context_api/ViewMenuCtx.js
@@ -0,0 +1,12 @@
+import React, { createContext, useState } from 'react'
+export const ViewMenuContext = createContext("");
+const ViewMenuProvider = () => {
+    const [data, setData] = useState([]);
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default ViewMenuProvider
